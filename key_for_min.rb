bcf67670a068122c090245fbee4cea545763b95f
Emily Asch <e.asch@me.com>
2018-10-16 21:10:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,4 +12,12 @@ def key_for_min_value(name_hash)
     min_num = array[0]
     array.each do |value|
       min_num = value if value < min_num
+    end
+    name_hash.each do |key, value|
+      if min_num == value 
+        return key
+      end
+    end
+  end
+      
 end
